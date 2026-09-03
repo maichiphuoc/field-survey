3644e750e41a2d5abc92132d4e64c7ece01124f1
CPhuoc <phuocmc.23itb@vku.udn.vn>
2026-09-03 15:47:03 +0700

    Enable camera on web

--- vku-field-survey/src/main.ts
+++ vku-field-survey/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import './style.css';
 import App from './App.vue';
+
+defineCustomElements(window);
 
 createApp(App).mount('#app');
