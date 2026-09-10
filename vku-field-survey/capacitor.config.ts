b3ec65ae26345af6080379b28d5f269af3094e2c
CPhuoc <phuocmc.23itb@vku.udn.vn>
2026-09-10 20:47:22 +0700

    Update VKU field survey Android app

--- vku-field-survey/capacitor.config.ts
+++ vku-field-survey/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'vku-field-survey',
+  appId: 'vn.edu.vku.fieldsurvey',
+  appName: 'VKU Field Survey',
   webDir: 'dist'
 };
 
